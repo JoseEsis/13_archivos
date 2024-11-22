dd023ae90f31182cece40dd37cc2e1452b99202e
JoseEsis <japomap@unjbg.edu.pe>
2024-11-22 15:23:19 -0500

    Agregar Lectura de un archivo de texto

---
+++ textoLeer.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+	
+	const int IGV = 21;
+	string ruc, producto, texto;
+	int unidades;
+	double precio, neto, total, igv;
+	char aux;
+	ifstream archivo;
+	int contador = 0; 
+	
+	archivo.open("compras.txt");
+		
+	if(archivo.is_open())	
+	{            
+		archivo>>ruc;
+		while(ruc != "X")
+		{
+			archivo >> unidades >> precio;
+			archivo.get(aux);
+			getline(archivo, producto);
+			contador++;
+			neto = unidades * precio;
+			igv = neto * IGV/100;
+			total = neto + igv;
+			cout<<"Compra "<<contador<<".-"<<endl;
+			cout<<"    "<<producto<<": "<<unidades
+				<<" x "<<fixed<<setprecision(2)
+				<<precio<<" = "<<neto<<" - I.G.V.: "
+				<<igv<<" - Total: "<<total<<endl;
+			archivo>>ruc;
+		}
+			archivo.close();		
+		
+	}
+	else
+	{
+		cout<<"ERROR: No se ha podido abrir el archivo"<<endl;	
+	}		
+	return 0;                                              
+} 
